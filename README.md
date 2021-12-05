ca77ec332f89acee4029dfff697df48040e89b88
largentba21 <largentba21@gmail.com>
2021-12-05 12:46:02 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# DNDBeyond Character Sheet Spell Source Column
+
+A module used to inject a spell source column into the DNDBeyond Character Sheet for use by the Spellbook Assistant Manager module.
---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+Hooks.on("renderActorSheet", (actor, html) => {
+  const headers = html.find(".spellbook .inventory-header .spell-school");
+  for (const header of headers) {
+    const $sourceClass = $(`<div class="source-class">Source Class</div>`);
+    header.after($sourceClass[0]);
+  }
+
+  const spells = html.find(
+    ".spellbook .item:not('.inventory-header') .spell-school"
+  );
+  for (const spell of spells) {
+    const id = spell.parentElement.outerHTML.match(/data-item-id="(.*?)"/);
+    const item = actor.object.items.get(id[1]);
+    const itemClass = item.data.flags["spellbook-assistant-manager"]?.class;
+    const classItem =
+      actor.object.classes[`${itemClass?.slugify({ strict: true })}`];
+    const $sourceClass = $(`
+          <div class="source-class">
+              <div class="class-image" aria-label="${
+                classItem?.name || ""
+              }" style="background-image: url(${classItem?.img || ""});"></div>
+          </div>
+              `);
+    spell.after($sourceClass[0]);
+  }
+});
